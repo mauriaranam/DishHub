afe233bb409781607339dc4ef2faf21345ac4294
Mauricio Arana <mauriaranam@gmail.com>
2023-06-30 15:08:04 -0400

    Actualizacion pequenha en models.py

--- models.py
+++ models.py
@@ -5,15 +5,23 @@ db = SQLAlchemy()
 
 #Creamos nuestros modelos(Tablas)
 class User(db.Model):
-    id_user = db.Column(db.Integer, primary_key=True)
+    id_usuario = db.Column(db.Integer, primary_key=True)
     usuario = db.Column(db.String(12), unique=True, nullable=False)
     password = db.Column(db.String(12))
     correo_user = db.Column(db.String, unique = True, nullable = False)
+    nombre = db.Column(db.String(12))
+    apellido = db.Column(db.String(12))
     #foto_usuario no se como poner en una db
 
 class Receta(db.Model):
     id_receta = db.Column(db.Integer, primary_key=True)
-    nombre_receta = db.Column(db.String(12), unique=True, nullable=False)
-    descripcion_receta = db.Column(db.String(12))
+    id_usuario = db.Column(db.Integer, foreign_key=True)
+    nombre_receta = db.Column(db.String(25), unique=True, nullable=False)
+    descripcion_receta = db.Column(db.String(25))
+    ingredientes = db.Column(db.List)
+    calificacion_receta = db.Column(db.Integer)
+    #foto_receta no se como poner en una db
+    fecha_receta = db.Column(db.String)
+
 
     #Iterar la lista de ingredientes dps
