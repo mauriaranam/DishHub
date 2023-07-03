7d84db560deb42b0af4dc531d46d34f83c424577
Mauricio Arana <mauriaranam@gmail.com>
2023-07-03 13:47:45 -0400

    Actualizaciones dentro de las db y templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #Importancion de librerias  
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 # Importamos los modelos de la tablas
 from models import db, User, Receta
 
@@ -17,7 +17,7 @@ db.init_app(app)
 def index():
     return render_template("home.html")
 
-@app.route("/register", methods = ["POST", "GET"])
+@app.route("/register", methods=["POST", "GET"])
 def register():
     #Recibimos los datos del Front
     if request.method == 'POST':
@@ -26,39 +26,52 @@ def register():
         password = request.form ["password"]
         nombre = request.form ["nombre"]
         apellido = request.form ["apellido"]
-        
-        usuario = User(username, email, password, nombre, apellido)
+        usuario = User(username=username, correo_user=email, password=password, nombre=nombre, apellido=apellido)
         #Agregamos a la db
         #Agrego con
         db.session.add(usuario)
         #Y confirmo con
         db.session.commit()
-        #Ver como era
-        #global current_user
-        #current_user = id_usuario
-        return 'Agregado correctamente'
+        global current_user
+        current_user = User.id
+        return redirect(url_for("login"))
     return render_template ("register.html")
 
-@app.route("/receta", methods = ["POST", "GET"])
-def receta_nueva():
+@app.route("/receta", methods=["POST", "GET"])
+def receta():
     #Recibimos los datos del Front
     if request.method == 'POST':
         nombre_receta = request.form ["nombre_receta"]
         descripcion_receta = request.form ["descripcion_receta"]
-        user_id = request.form["user_id"]
-        
-        receta_de_usuario = Receta(nombre_receta,descripcion_receta,user_id)
-        #Agregamos a la db
-        #Agrego con
+        global current_user 
+        user_id = current_user
+        receta_de_usuario = Receta(nombre_receta=nombre_receta,descripcion_receta=descripcion_receta, user_id=user_id)
         db.session.add(receta_de_usuario)
-        #Y confirmo con
         db.session.commit()
-        #Ver como era
-        #global current_user
-        #current_user = id_usuario
-        return 'Agregado correctamente'
+        return redirect(url_for("home"))
     return render_template ("receta.html")
 
+
+@app.route("/home")
+def home():
+    return render_template ("home.html")
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        correo_user = request.form ['correo_user']
+        password = request.form ['password']
+        usuario_db = User.query.filter_by(correo_user=correo_user).first()
+        if usuario_db is not None:
+            if usuario_db.password == password:
+                global current_user 
+                current_user = usuario_db.id
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('login'))
+        elif usuario_db is None:
+            return redirect(url_for('login'))  
+    return render_template('login.html')
 
 ## Breakpoint ##
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -5,13 +5,13 @@ db = SQLAlchemy()
 
 #Creamos nuestros modelos(Tablas)
 class User(db.Model):
-    id_usuario = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(12), unique=True, nullable=False)
     correo_user = db.Column(db.String, unique = True, nullable = False)
     password = db.Column(db.String(12))
     nombre = db.Column(db.String(12))
     apellido = db.Column(db.String(12))
-    receta = db.relationship('Receta', backref = 'user')
+    recetas = db.relationship('Receta', backref = 'user')
 
     def __init__ (self, username, correo_user, password, nombre,apellido):
         self.username = username
@@ -21,16 +21,16 @@ class User(db.Model):
         self.apellido = apellido
 
 class Receta(db.Model):
-    id_receta = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nombre_receta = db.Column(db.String(25))
     descripcion_receta = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-    def __init__ (self, nombre_receta, descripcion_receta, user_id):
+    def __init__ (self, nombre_receta, descripcion_receta):
         self.nombre_receta = nombre_receta
         self.descripcion_receta = descripcion_receta
-        self.user_id = user_id
+        #self.user_id = user_id
 
     #calificacion_receta = db.Column(db.Integer)
     #fecha_receta = db.Column(db.String)
