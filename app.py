980aaed191f5be8c5994c68b67c1d01889e018c1
Mauricio Arana <mauriaranam@gmail.com>
2023-07-10 02:22:10 -0400

    little fixes

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from PIL import Image
 import re
 # Importamos openai
 import openai
-openai.api_key = "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"
+openai.api_key = "your api key"
 
 
 # Instanciamos Flask
@@ -75,7 +75,8 @@ def buscar_alexia():
         6. Si el usuario tiene alguna pregunta técnica sobre técnicas culinarias, proporciona explicaciones claras y consejos útiles.
         7. Utiliza un tono amigable y cercano para interactuar con los usuarios.
         8. Siempre verifica la comprensión de la consulta del usuario antes de proporcionar una respuesta.
-        9. Siempre saluda y pregunta antes de darle alguna indicacion
+        9. Siempre saluda
+        10. Si el usuario solo ingresa ingredientes, brinda recetas con los ingredientes disponibles
         Chef AlexIA: {pregunta}
         """
         pregunta = f"{ingredientes_usuario}"
